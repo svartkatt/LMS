2784f7886c6637d95502255fe9678e23487dc15e
Anton <kagalanton@mail.com>
2021-02-17 16:51:21 +0200

    homework_08

--- academy/urls.py
+++ academy/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students', views.students),
-    path('lecturers', views.lecturers),
-    path('groups', views.groups),
+    path('students', views.students, name='students'),
+    path('students/create_students', views.create_students, name='create_students'),
+    path('students/edit/<int:id>', views.edit_students, name='edit_students'),
+    path('students/delete/<int:id>', views.delete_student, name='delete_student'),
+    path('lecturers', views.lecturers, name='lecturers'),
+    path('lecturers/create_lecturers', views.create_lecturers, name='create_lecturers'),
+    path('lecturers/edit/<int:id>', views.edit_lecturers, name='edit_lecturers'),
+    path('lecturers/delete/<int:id>', views.delete_lecturer, name='delete_lecturer'),
+    path('groups', views.groups, name='groups'),
+    path('groups/create_groups', views.create_groups, name='create_groups'),
+    path('groups/edit/<int:id>', views.edit_groups, name='edit_groups'),
+    path('groups/delete/<int:id>', views.delete_groups, name='delete_groups'),
 ]
