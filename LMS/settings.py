3449b46a0daa60cce8d386e412444bddf92c2919
Anton <kagalanton@mail.com>
2021-02-19 23:17:46 +0200

    homework_09

--- LMS/settings.py
+++ LMS/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'academy',
+    'academy.apps.AcademyConfig',
     'silk',
     'logger'
-  #  'academy.apps.AcademyConfig',
 ]
 
 MIDDLEWARE = [
--- academy/signals.py
+++ academy/signals.py
@@ -5,6 +5,10 @@ from django.dispatch import receiver
 @receiver(pre_save, sender='Student')
 @receiver(pre_save, sender='Lecturer')
 def capitalize_names(instance, **kwargs):
-    instance.first_name = instance.first_name + 'fdagdfs'
+    instance.first_name = instance.first_name.capitalize()
     instance.last_name = instance.last_name.capitalize()
-    instance.capitalize().save()
+
+
+@receiver(pre_save, sender='Group')
+def capitalize_names(instance, **kwargs):
+    instance.course = instance.course.capitalize()
