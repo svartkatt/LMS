633fe5e264b97344460eb1d5e670e5c6c766fbe3
Anton <kagalanton@mail.com>
2021-02-28 19:58:32 +0200

    homework_10_re

--- LMS/celery.py
+++ LMS/celery.py
@@ -2,6 +2,8 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LMS.settings')
 
 app = Celery('LMS')
@@ -12,3 +14,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'delete_logs': {
+        'task': 'logger.tasks.delete_logs',
+        'schedule': crontab(hour=24)
+    }
+}
