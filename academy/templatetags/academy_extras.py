e017c926a2b18619e6cb1edc17bf8682e595d6ae
Anton <kagalanton@mail.com>
2021-03-17 22:37:14 +0200

    homework_12

--- academy/templatetags/academy_extras.py
+++ academy/templatetags/academy_extras.py
@@ -4,12 +4,8 @@ register = template.Library()
 
 
 @register.filter()
-def get_reading_time(value, step):
-    if step not in ['sec', 'min']:
-        raise ValueError('Invalid step')
-
-    words_count = len(value.split(' '))
-    reading_time_min = words_count / 120
-    reading_time = reading_time_min if step == 'min' else reading_time_min * 60
-
-    return f'{round(reading_time)} {step} to read'
+def get_count(value):
+    count = 0
+    for i in value:
+        count += 1
+    return count
