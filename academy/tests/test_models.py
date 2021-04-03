af0e828ac655f2ec26076044992f544a20058858
Anton <kagalanton@mail.com>
2021-04-03 20:59:41 +0300

    homework_19_temp

--- academy/tests/test_models.py
+++ academy/tests/test_models.py
@@ -1,85 +1,69 @@
-import contextlib
-
-from django.core.exceptions import ValidationError
-from django.test import TestCase
+import pytest
 
 from academy.models import Student, Lecturer, Group
 
 
-class AcademyModelTest(TestCase):
+@pytest.mark.django_db
+def test_successful_student_creation():
+    Student.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    assert Student.objects.count() == 1
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.text = 'Some text'
-        cls.mail = 'somemail@gmail.com'
-        cls.student = Student.objects.create(
-            first_name='some_name',
-            last_name='some_name',
-            email='somemail@gmail.com'
-        )
 
-        cls.lecturer = Lecturer.objects.create(
-            first_name='some_name',
-            last_name='some_name',
-            email='somemail@gmail.com'
-        )
+@pytest.mark.django_db
+def test_failure_due_to_student_long_first_name():
+    long_name = 'a' * 31
+    student = Student.objects.create(first_name=long_name, last_name='Some name', email='somemail@gmail.com')
+    assert len(student.first_name) <= 30
 
-    def setUp(self) -> None:
-        print('setup')
 
-    def tearDown(self) -> None:
-        print('teardown')
+# @pytest.mark.django_db
+# def test_failure_due_to_student_mail_is_none():
+#     student = Student.objects.create(first_name='Some name', last_name='Some name', email=None)
+#     assert student.full_clean() is False
 
-    def test_successful_student_creation(self):
-        student = Student(first_name=self.text, last_name=self.text, email=self.mail)
-        student.full_clean()
 
-    def test_failure_due_to_student_long_first_name(self):
-        long_name = 'a' * 31
-        student = Student(first_name=long_name, last_name=self.text, email=self.mail)
-        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
-        with self.assertRaisesMessage(ValidationError, expected_message):
-            student.full_clean()
+@pytest.mark.django_db
+def test_successful_lecturer_creation():
+    Lecturer.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    assert Lecturer.objects.count() == 1
 
-    def test_failure_due_to_student_mail_is_none(self):
-        student = Student(first_name=self.text, last_name=self.text, email=None)
-        expected_message = 'This field cannot be null.'
-        with self.assertRaisesMessage(ValidationError, expected_message):
-            student.full_clean()
 
-    def test_successful_lecturer_creation(self):
-        lecturer = Lecturer(first_name=self.text, last_name=self.text, email=self.mail)
-        lecturer.full_clean()
+@pytest.mark.django_db
+def test_failure_due_to_lecturer_long_first_name():
+    long_name = 'a' * 31
+    lecturer = Lecturer.objects.create(first_name=long_name, last_name='Some name', email='somemail@gmail.com')
+    assert len(lecturer.first_name) <= 30
 
-    def test_failure_due_to_lecturer_long_second_name(self):
-        long_name = 'a' * 31
-        lecturer = Lecturer(first_name=self.text, last_name=long_name, email=self.mail)
-        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
-        with self.assertRaisesMessage(ValidationError, expected_message):
-            lecturer.full_clean()
 
-    def test_failure_due_to_lecturer_mail_is_none(self):
-        lecturer = Lecturer(first_name=self.text, last_name=self.text, email=None)
-        expected_message = 'This field cannot be null.'
-        with self.assertRaisesMessage(ValidationError, expected_message):
-            lecturer.full_clean()
+# @pytest.mark.django_db
+# def test_failure_due_to_lecturer_mail_is_none():
+#     lecturer = Lecturer.objects.create(first_name='Some name', last_name='Some name', email=None)
 
-    def test_successful_group_creation(self):
-        group = Group.objects.create(course=self.text, teacher=self.lecturer)
-        group.students.add(self.student)
-        group.full_clean()
 
-    def test_failure_due_to_group_long_name(self):
-        long_name = 'a' * 31
-        group = Group.objects.create(course=long_name, teacher=self.lecturer)
-        group.students.add(self.student)
-        expected_message = 'Ensure this value has at most 30 characters (it has 31).'
-        with self.assertRaisesMessage(ValidationError, expected_message):
-            group.full_clean()
+@pytest.mark.django_db
+def test_successful_group_creation():
+    lecturer = Lecturer.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    student = Student.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    group = Group.objects.create(course='Some name', teacher=lecturer)
+    group.students.add(student)
+    assert Lecturer.objects.count() == 1
 
-    @contextlib.contextmanager
-    def test_to_dict_equals_to_short_representation(self):
-        group = Group.objects.create(course=self.text, teacher=self.lecturer)
-        group.students.add(self.student)
-        expected = {'course': self.text, 'teacher': self.lecturer}
-        self.assertEquals(group.to_dict(), expected)
+
+@pytest.mark.django_db
+def test_failure_due_to_group_long_name():
+    long_name = 'a' * 31
+    lecturer = Lecturer.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    student = Student.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    group = Group.objects.create(course=long_name, teacher=lecturer)
+    group.students.add(student)
+    assert len(group.course) <= 30
+
+
+@pytest.mark.django_db
+def test_to_dict_equals_to_short_representation():
+    lecturer = Lecturer.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    student = Student.objects.create(first_name='Some name', last_name='Some name', email='somemail@gmail.com')
+    group = Group.objects.create(course='Some name', teacher=lecturer)
+    group.students.add(student)
+    expected = {'course': 'Some name', 'teacher': lecturer}
+    assert group.to_dict() == expected
--- academy/tests/test_views.py
+++ academy/tests/test_views.py
@@ -1,44 +1,59 @@
-# from django.test import TestCase
+# import pytest
 # from django.urls import reverse
 #
 #
-# class ListViewTest(TestCase):
+# @pytest.mark.django_db
+# def test_view_students_url_exists_at_desired_location(client):
+#     resp = client.get('/students')
+#     assert resp.status_code, 200
 #
-#     def test_view_students_url_exists_at_desired_location(self):
-#         resp = self.client.get('/students')
-#         self.assertEqual(resp.status_code, 200)
 #
-#     def test_view_students_url_accessible_by_name(self):
-#         resp = self.client.get(reverse('students'))
-#         self.assertEqual(resp.status_code, 200)
+# @pytest.mark.django_db
+# def test_view_students_url_accessible_by_name(client):
+#     resp = client.get(reverse('students'))
+#     assert resp.status_code, 200
 #
-#     def test_view_uses_correct_students_template(self):
-#         resp = self.client.get(reverse('students'))
-#         self.assertEqual(resp.status_code, 200)
-#         self.assertTemplateUsed(resp, 'academy/students.html')
 #
-#     def test_view_lecturers_url_exists_at_desired_location(self):
-#         resp = self.client.get('/lecturers')
-#         self.assertEqual(resp.status_code, 200)
+# @pytest.mark.django_db
+# def test_view_uses_correct_students_template(client):
+#     resp = client.get(reverse('students'))
+#     assert resp.status_code, 200
+#     assert resp, 'academy/students.html'
 #
-#     def test_view_lecturers_url_accessible_by_name(self):
-#         resp = self.client.get(reverse('lecturers'))
-#         self.assertEqual(resp.status_code, 200)
 #
-#     def test_view_uses_correct_lecturers_template(self):
-#         resp = self.client.get(reverse('lecturers'))
-#         self.assertEqual(resp.status_code, 200)
-#         self.assertTemplateUsed(resp, 'academy/lecturers.html')
+# @pytest.mark.django_db
+# def test_view_lecturers_url_exists_at_desired_location(client):
+#     resp = client.get('/lecturers')
+#     assert resp.status_code, 200
 #
-#     def test_view_groups_url_exists_at_desired_location(self):
-#         resp = self.client.get('/groups')
-#         self.assertEqual(resp.status_code, 200)
 #
-#     def test_view_groups_url_accessible_by_name(self):
-#         resp = self.client.get(reverse('groups'))
-#         self.assertEqual(resp.status_code, 200)
+# @pytest.mark.django_db
+# def test_view_lecturers_url_accessible_by_name(client):
+#     resp = client.get(reverse('lecturers'))
+#     assert resp.status_code, 200
 #
-#     def test_view_uses_correct_groups_template(self):
-#         resp = self.client.get(reverse('groups'))
-#         self.assertEqual(resp.status_code, 200)
-#         self.assertTemplateUsed(resp, 'academy/groups.html')
+#
+# @pytest.mark.django_db
+# def test_view_uses_correct_lecturers_template(client):
+#     resp = client.get(reverse('lecturers'))
+#     assert resp.status_code, 200
+#     assert resp, 'academy/lecturers.html'
+#
+#
+# @pytest.mark.django_db
+# def test_view_groups_url_exists_at_desired_location(client):
+#     resp = client.get('/groups')
+#     assert resp.status_code, 200
+#
+#
+# @pytest.mark.django_db
+# def test_view_groups_url_accessible_by_name(client):
+#     resp = client.get(reverse('groups'))
+#     assert resp.status_code, 200
+#
+#
+# @pytest.mark.django_db
+# def test_view_uses_correct_groups_template(client):
+#     resp = client.get(reverse('groups'))
+#     assert resp.status_code, 200
+#     assert resp, 'academy/groups.html'
