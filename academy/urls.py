c1a0e1a09e1499d20ce9d8b1f1aa7a93e5739721
Anton <kagalanton@mail.com>
2021-04-19 15:18:12 +0300

    homework_22

--- academy/urls.py
+++ academy/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
 from . import views
+from .views import StudentsCreateView, LecturersCreateView, GroupsCreateView, GroupsEditView, LecturersEditView, \
+    StudentsEditView, StudentsDeleteView, GroupsDeleteView, LecturersDeleteView
 
 urlpatterns = [
     path('', views.students, name='students'),
     path('students', views.students, name='students'),
-    path('students/create_students', views.create_students, name='create_students'),
-    path('students/edit/<int:id>', views.edit_students, name='edit_students'),
-    path('students/delete/<int:id>', views.delete_student, name='delete_student'),
+    path('students/create_students', StudentsCreateView.as_view(), name='create_students'),
+    path('students/edit/<int:pk>', StudentsEditView.as_view(), name='edit_students'),
+    path('students/delete/<int:pk>', StudentsDeleteView.as_view(), name='delete_student'),
     path('lecturers', views.lecturers, name='lecturers'),
-    path('lecturers/create_lecturers', views.create_lecturers, name='create_lecturers'),
-    path('lecturers/edit/<int:id>', views.edit_lecturers, name='edit_lecturers'),
-    path('lecturers/delete/<int:id>', views.delete_lecturer, name='delete_lecturer'),
+    path('lecturers/create_lecturers', LecturersCreateView.as_view(), name='create_lecturers'),
+    path('lecturers/edit/<int:pk>', LecturersEditView.as_view(), name='edit_lecturers'),
+    path('lecturers/delete/<int:pk>', LecturersDeleteView.as_view(), name='delete_lecturer'),
     path('groups', views.groups, name='groups'),
-    path('groups/create_groups', views.create_groups, name='create_groups'),
-    path('groups/edit/<int:id>', views.edit_groups, name='edit_groups'),
-    path('groups/delete/<int:id>', views.delete_groups, name='delete_groups'),
+    path('groups/create_groups', GroupsCreateView.as_view(), name='create_groups'),
+    path('groups/edit/<int:pk>', GroupsEditView.as_view(), name='edit_groups'),
+    path('groups/delete/<int:pk>', GroupsDeleteView.as_view(), name='delete_groups'),
     path('contact_us', views.contact_us, name='contact_us'),
 ]
